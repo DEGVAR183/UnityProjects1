75276ebe4c98f4de5a7b6122319c325b704edaf1
DEGVAR183 <larindenus@gmail.com>
2020-07-25 21:58:33 +0300

    Теперь музыка играет на всех сценах

--- Assets/Scripts/splashScreen.cs
+++ Assets/Scripts/splashScreen.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class splashScreen : MonoBehaviour
 {
     // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     void Start()
     {
-        
+        Invoke("FirstLevelLoad", 2f);
     }
 
     // Update is called once per frame
-    void Update()
+    void FirstLevelLoad()
     {
-        
+        SceneManager.LoadScene(1);
     }
 }
