545c5a01b0c9b109bfd359e6cd9bf6ee920111a5
DEGVAR183 <larindenus@gmail.com>
2020-08-06 19:40:24 +0300

    Использовал кросплатформенное управления, сделал скрипт Player, и сделал управления по горизонтали

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("м/с")][SerializeField] float xSpeed = 20f;
+    [SerializeField] float XClamp = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xMove * xSpeed * Time.deltaTime;
+        float newXPos = transform.localPosition.x + xOffset;
+        float clampXPos = Mathf.Clamp(newXPos, -XClamp, XClamp);
+
+        transform.localPosition = new Vector3(clampXPos, transform.localPosition.y,transform.localPosition.z);
+
+
+    }
+}
