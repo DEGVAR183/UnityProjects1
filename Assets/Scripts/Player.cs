e457123f537a923032c3afb8b6cae87217b1fd15
DEGVAR183 <larindenus@gmail.com>
2020-08-07 10:25:09 +0300

    Сделал перемещения корабля по вертикали

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("м/с")][SerializeField] float xSpeed = 20f;
-    [SerializeField] float XClamp = 5f;
+    [Tooltip("м/с")][SerializeField] float xSpeed = 4f;
+    [Tooltip("м/с")] [SerializeField] float ySpeed = 4f;
+
+    [SerializeField] float XClamp = 20f;
+    [SerializeField] float YClamp = 8.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +21,16 @@ public class Player : MonoBehaviour
     void Update()
     {
         float xMove = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yMove = CrossPlatformInputManager.GetAxis("Vertical");
         float xOffset = xMove * xSpeed * Time.deltaTime;
+        float yOffset = yMove * ySpeed * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
         float clampXPos = Mathf.Clamp(newXPos, -XClamp, XClamp);
 
-        transform.localPosition = new Vector3(clampXPos, transform.localPosition.y,transform.localPosition.z);
+        float newYPos = transform.localPosition.y + yOffset;
+        float clampYPos = Mathf.Clamp(newYPos, -YClamp, YClamp);
+
+        transform.localPosition = new Vector3(clampXPos, clampYPos,transform.localPosition.z);
 
 
     }
