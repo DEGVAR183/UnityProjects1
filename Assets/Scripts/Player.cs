f9fb60021af29971ffb1b35843f3a659bd8d9bfc
DEGVAR183 <larindenus@gmail.com>
2020-08-11 21:25:27 +0300

    сделал покачивания при перемещении

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,16 @@ public class Player : MonoBehaviour
     [Tooltip("м/с")][SerializeField] float Speed = 4f;
     
 
-    [SerializeField] float XClamp = 20f;
-    [SerializeField] float YClamp = 8.5f;
+    [SerializeField] float XClamp = 30f;
+    [SerializeField] float YClamp = 10f;
+    [SerializeField] float xRotFactor = -1.8f;
+    [SerializeField] float yRotFactor = 1.5f;
+    [SerializeField] float zRotFactor = -1.5f;
+    [SerializeField] float xMoveRot = -10f;
+    [SerializeField] float yMoveRot = 10f;
+    [SerializeField] float zMoveRot = 10f;
 
+    float xMove, yMove;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,8 +33,8 @@ public class Player : MonoBehaviour
     }
     void MoveShip()
     {
-        float xMove = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yMove = CrossPlatformInputManager.GetAxis("Vertical");
+        xMove = CrossPlatformInputManager.GetAxis("Horizontal");
+        yMove = CrossPlatformInputManager.GetAxis("Vertical");
         float xOffset = xMove * Speed * Time.deltaTime;
         float yOffset = yMove * Speed * Time.deltaTime;
         float newXPos = transform.localPosition.x + xOffset;
@@ -41,6 +48,9 @@ public class Player : MonoBehaviour
     }
     void RotateShip()
     {
-        transform.localRotation = Quaternion.Euler(-30,30,0);
+        float xRot = transform.localPosition.y * xRotFactor + yMove * xMoveRot;
+        float yRot = transform.localPosition.x * yRotFactor + xMove * yMoveRot;
+        float zRot = xMove * zMoveRot;
+        transform.localRotation = Quaternion.Euler(xRot,yRot,zRot);
     }
 }
