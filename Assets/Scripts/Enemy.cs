31a52488fa357a57b369029d3a2d5d7df16188d5
DEGVAR183 <larindenus@gmail.com>
2020-08-31 14:42:19 +0300

    Добавляю BoxCollider на Enemy через скрипт

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerCollider();
+    }
+
+    // Update is called once per frame
+    void AddNonTriggerCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+        
+    }
+}
