302297eea168a12c1d0cd61731e1eb216e7215e2
DEGVAR183 <larindenus@gmail.com>
2020-08-31 11:05:54 +0300

    Настроил эфект взрыва, и переход на сл. сцену

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("В секундах")][SerializeField] float LoadLevelDelay;
+    [Tooltip("Префаб эффекта взрыва")] [SerializeField] GameObject explosionFX;
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         print("Hit Trigger");
         StartDeath();
+        explosionFX.SetActive(true);
+        Invoke("RestartLevel", LoadLevelDelay);
     }
     void StartDeath()
     {
         SendMessage("OnPlayerDeath");
+    }
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 }
